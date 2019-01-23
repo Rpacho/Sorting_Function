b4217fdb9d9c4fb9cfcc3db25db96046458de49b
Rolando <46900743+Rpacho@users.noreply.github.com>
2019-01-23 13:42:35 -0800

    initial file
    purpose is to sort array using different sorting function.

---
+++ SortFunction/Selection.java
@@ -0,0 +1,25 @@
+public class Selection
+{
+    
+    public void selectionSort(int[] array)
+    {
+        int smallIndex;
+        int temp;
+        int numbers;
+        for (int i = 0; i < array.length-1; i++)
+        {
+            smallIndex = i;
+            for (int j = 0; j < array.length; j++)
+            {
+                if (array[j] < array[smallIndex]){
+                    smallIndex = j;
+                } 
+            }
+            temp = 
+        }
+    }
+
+    public static void main(String[] args){
+
+    }
+}
