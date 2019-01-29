7c03036bef3958d5a3c526b76462dbeceb6e2c23
Rolando <46900743+Rpacho@users.noreply.github.com>
2019-01-29 11:07:06 -0800

    added Insertion sorter

---
+++ SortFunction/Insertion.java
@@ -0,0 +1,43 @@
+
+// This is another sorting function
+/*
+ *Author: Rolando Pacho
+ *Param
+ *
+ *
+ */
+
+public class Insertion
+{
+	public static void sortInsertion(int[] array)
+	{
+		int j;		
+		int temp;
+		for(int i = 0; i < array.length; i++)
+		{
+			// put j index the same as i
+			j = i;
+			while ((j > 0) && (array[j] < array[j-1]))
+			{
+				// do the swap
+				temp = array[j-1];
+				array[j-1] = array[j];
+				array[j] = temp;
+				j--;	
+			}	
+		}
+		for(int i = 0; i < array.length; i++)
+		{
+			System.out.print(array[i] + " ");
+		}
+			
+		System.out.println();
+	}
+
+	public static void main(String[] args)
+	{
+		int[] array = {37, 55, 2, 9, 14, 7, 6, 7};
+		sortInsertion(array);
+	}
+}
+
